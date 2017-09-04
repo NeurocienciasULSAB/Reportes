1410c2214a5fe508b04510ca674dbed5a5065220
Alberto Barradas <abcsds@gmail.com>
2017-09-03 20:34:29 -0500

    Add read_chsetup file

--- files.jl
+++ files.jl
@@ -1,5 +1,5 @@
 """
-`read_sig(path::String, n_channels::Int, [header::Array{String})], sep::Char='\t', rem_len::Int=5`
+`read_sig(path::String, n_channels::Int, [header::Array{String})], sep::Char='\t', rem_len::Int=5)`
 Reads numeric signals from a text file and returns them and their header.
 """
 function read_sig(path::String, n_channels::Int, sep::Char='\t', rem_len::Int=5)
@@ -29,4 +29,41 @@ function read_sig(path::String, n_channels::Int, header::Array{String}, sep::Cha
     header = Array{String}([i[1:end-(rem_len)] for i in header]) # Remove reference name ex 'A1A2'
     header = header[1,:]
     return sig, header
+end
+
+"""
+`read_setup([path::String, sep::Char='\t'])`
+Reads a channel setup from a tsv file. Columns are x-position, y-position, and channel name. Defaults to nicolette 19 channel 10-20 setup
+"""
+function read_chsetup()
+    setup = [0.3   1.0    "FP1";
+             0.7   1.0    "FP2";
+             0.25  0.775  "F3" ;
+             0.75  0.775  "F4" ;
+             0.5   0.725  "FZ" ;
+             0.25  0.5    "C3" ;
+             0.75  0.5    "C4" ;
+             0.5   0.5    "CZ" ;
+             0.25  0.225  "P3" ;
+             0.75  0.225  "P4" ;
+             0.5   0.275  "PZ" ;
+             0.3   0.0    "O1" ;
+             0.7   0.0    "O2" ;
+             0.0   0.85   "F7" ;
+             1.0   0.85   "F8" ;
+             0.0   0.5    "T3" ;
+             1.0   0.5    "T4" ;
+             0.0   0.15   "T5" ;
+             1.0   0.15   "T6" ]
+end
+
+function read_chsetup(path::String, sep::Char='\t')
+    setup = []
+    try
+        setup = readdlm(path,sep,header=false)
+    catch err
+        error("Couldn't find setup, using fallback 19ch.")
+        return read_chsetup()
+    end
+    return setup
 end
--- main.jl
+++ main.jl
@@ -1,15 +1,24 @@
 module Reporte
-export read_sig, pot_abs, pot_rel, coh
+export read_sig,
+    read_chsetup,
+    pot_abs,
+    pot_rel,
+    coh,
+    headplot
+
 include("files.jl")
 include("pot.jl")
 include("coh.jl")
+include("plots.jl")
 end
 
 using Reporte
 
-Sample_Frequency = 500
-bands = ["delta","theta","alpha1","alpha2","beta1","beta2","gamma"]
-n_channels = 19
+setup = read_chsetup()
+band_names = ["delta","theta","alpha1","alpha2","beta1","beta2","gamma"]
+
+# Sample_Frequency = 500
+# n_channels = 19
 
 
 # sig, channels = read_sig(joinpath(pwd(),"example/eeg.txt"), n_channels)
@@ -19,5 +28,4 @@ n_channels = 19
 # println(sum(rel,2))
 # cor_mat  = cor(sig)
 # coh_mats = coh(sig,n_channels)
-
 
